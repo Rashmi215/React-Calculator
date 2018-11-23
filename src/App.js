6a22421db9ca96510fc2e31a3eb8df36c5b328ab
Rashmi Singh <rashmi.singh@newgen.co.in>
2018-11-23 17:44:52 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ResultComponent from './components/result_component';
+import KeypadComponent from './components/keypad_component';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state= {result: ''};
+    this.baseState= this.state
+  }
+
+  calculate= ()=>{
+     try{
+       this.setState({ result: (eval(this.state.result) || "") + "" });
+     }catch(e){
+       this.setState({ result: "error" });
+     }
+  };
+
+  backspace= ()=>{
+    this.setState({ result: this.state.result.slice(0,-1) });
+  };
+
+  // reset= ()=>{
+  //   this.setState({ result: " " });
+  // };
+
+  onClick= (button)=>{
+    if(button === '='){
+      this.calculate();
+    }else if(button === "C"){
+      this.setState(this.baseState);
+    }else if(button === "CE"){
+      this.backspace();
+    }else{
+      this.setState({ result: this.state.result + button });
+    }
+  };
+
+  render(){
+    return(
+      <div className= "calculator-body">
+         <div>
+            <ResultComponent result= {this.state.result} />
+            <KeypadComponent onClick= {this.onClick} />
+         </div>
       </div>
     );
   }
